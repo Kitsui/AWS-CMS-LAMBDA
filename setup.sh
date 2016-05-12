c585cf643d1e875a63b17532eae9c8605b56e8f2
Obj3ct <Adam@Adams-MacBook-Pro.local>
2016-05-12 18:10:27 +1200

    Upload basic HTML functionality
    Added functionality to automate upload of index and error HTML, and
    grant public read on files

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 # Variables
-STACK_NAME="MyStack"
+STACK_NAME = "MyStack"
+BUCKET_NAME = "adam-miguel-bash-bucket"
 
 ECHO "--- SCRIPT START ---"
 
 # Create CloudFormation Stack (s3 Bucket)
 aws cloudformation create-stack --stack-name $STACK_NAME \
 --template-body file://scripts/cloudformation/initFramework.json \
---parameters file://scripts/cloudformation/templateParams.json
+--parameters [{ParameterKey=BucketName, ParameterKey=$BUCKET_NAME}]
+
+# Upload HTML to created bucket
+aws s3api put-object --bucket $BUCKET_NAME --key index.html  --body resources/HTML/index.html 
+aws s3api put-object --bucket $BUCKET_NAME --key error.html  --body resources/HTML/error.html
+
+# Set access control list on HTML
+aws s3api put-object-acl --bucket $BUCKET_NAME --key index.html --grant-read http://acs.amazonaws.com/groups/global/AllUsers
+aws s3api put-object-acl --bucket $BUCKET_NAME --key error.html--grant-read http://acs.amazonaws.com/groups/global/AllUsers
 
 ECHO "--- SCRIPT COMPLETE ---"
