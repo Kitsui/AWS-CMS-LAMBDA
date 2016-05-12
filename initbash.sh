20f6c8b7181cf144cd4ffbac920ab951c421839c
miggle <chownema@gmail.com>
2016-05-12 16:04:44 +1200

    init
    adding code to for loop concat

--- initbash.sh
+++ initbash.sh
@@ -4,16 +4,22 @@ source config.sh
 
 echo $startStmt
 
-a=(foo bar "foo 1" "bar two")  #create an array
-b=("${a[@]}")                  #copy the array in another one 
+a=()  #create an array
+                  #copy the array in another one 
+
+
+
+for file in /home/user/*; do
+  echo "files in directory :" ${file##*/} ,
+  b=("${a[@]}" ${file##*/})
+done
+
+echo completed
+echo 
 
 for value in "${b[@]}" ; do    #print the new array 
 echo "$value" 
 done 
-
-# for file in /home/user/*; do
-#   echo ${file##*/}
-# done
 
 
 echo $endStmt
