21ed643cf6e25cb96851e6e59f45735ce2c8ef77
miggle <chownema@gmail.com>
2016-05-18 22:59:01 +1200

    Fixed calling put item dynamo

--- scripts/Dynamo/dynamo.sh
+++ scripts/Dynamo/dynamo.sh
@@ -9,10 +9,5 @@ aws dynamodb create-table --table-name User \
  --key-schema AttributeName=UID,KeyType=HASH AttributeName=Username,KeyType=RANGE \
  --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
 
-# TODO: Miguel, need to figure out why the 
-# --item file causes an error when running via setup.sh
+source scripts/Dynamo/register-admin.sh
 
-# Insert admin into user table
-# aws dynamodb put-item --table-name User \
-# --item file://scripts/Dynamo/user.json \
-# --return-consumed-capacity TOTAL
