01922141facbcefa58fb4b847cb9ad809c310c0c
miggle <chownema@gmail.com>
2016-07-24 17:40:31 +1200

    response format changed to proper json - blog get all blogs

--- lambda/Response.py
+++ lambda/Response.py
@@ -16,18 +16,26 @@ class Response(object):
 			if(data["Items"][0] is not None):
 				self.columns = data["Items"][0].keys()
 
-	def format(self):
-		dct = {}
-		i = 0
-		items = []
-		columns = self.data["Items"][0].keys()
-		for item in self.data["Items"]:
-		    i += 1
-		    for col in columns:
-		        items.append({col : item[col]['S']})
-		        dct[i] = items
-	        self.data = dct
 
+	def format(self):
+		replyData = {}
+		colm = []
+		rows = []
+
+		item = {}
+		columns = self.data["Items"][0].keys()
+		for i in self.data["Items"]:
+			item["ID"] = (i['BlogID']['S'])
+			item["Title"] = (i['Title']['S'])
+			item["Author"] = (i['Author']['S'])
+			item["Content"] = (i['Content']['S'])
+			item["Meta_Description"] = (i['MetaDescription']['S'])
+			item["Meta_Keywords"] = (i['MetaKeywords']['S'])
+			item["Save_Date"] = (i['SavedDate']['S'])
+			rows.append(item)
+		replyData["rows"] = rows
+		replyData["columns"] = columns
+		return replyData
 
 	def to_JSON(self):
 		return jsonpickle.encode(self)
