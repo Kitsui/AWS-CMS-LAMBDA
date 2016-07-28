8420c95110cadce8cf5deb85549d72d818399966
Christopher Treadgold <christopher.treadgold@gmail.com>
2016-07-29 06:25:17 +1200

    Cleaned up validator.py

--- lambda/Validator.py
+++ lambda/Validator.py
@@ -1,10 +1,8 @@
 class Validator(object):
-
 	@staticmethod
 	def validateBlog(dataString):
-		caseIsNotFound = True
 		caseStrings = ["<script>", "<button>", "<form>", "<input>"]
 		for caseString in caseStrings:
-			if(dataString.find(caseString) != -1):
-				caseIsNotFound = False
-		return caseIsNotFound
+			if caseString in dataString:
+				return False
+		return True
