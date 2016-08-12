9bc2a3e09096d270a80c370b1df2a150dc5363dc
Adam Campbell <campbell.adam.31@gmail.com>
2016-08-12 17:40:24 +1200

    Added UI class, and proposal for UI requests
    Added UI class, and proposal for UI requests

---
+++ lambda/ui.py
@@ -0,0 +1,17 @@
+"""
+# ui.py
+# Author: Adam Campbell
+# Date: 12/08/2016
+"""
+
+class UI(object):
+
+    def __init__(self, event, context):
+        self.event = event
+        self.context = context
+        with open("constants.json", "r") as constants_file:
+            self.constants = json.loads(constants_file.read())
+
+    def getForm(self):
+        pass
+        
