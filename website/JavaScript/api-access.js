24e8772294453559868cfeb1e5cba447cf2c0fbd
Christopher-Treadgold <christopher.treadgold@gmail.com>
2016-09-08 16:13:34 +1200

    Added simple get blogs functionality, improved login.

--- website/JavaScript/api-access.js
+++ website/JavaScript/api-access.js
@@ -5,8 +5,40 @@ angular.module("apiAccess", [])
     "use strict";
     $httpProvider.defaults.withCredentials = true;
   })
-  .controller("login", ["$scope", "$http",
+  .controller("getBlogs", ["$scope", "$http",
     function ($scope, $http) {
       "use strict";
-      
+      $http.post(
+        "$(API_URL)",
+        {
+          "request": "getBlogs"
+        }
+      ).then(function successCallback(response) {
+        $scope.blogData = response.data.rows;
+      }, function errorCallback(response) {
+        $scope.test = response;
+      });
+    }])
+  .controller("uploadBlog", ["$scope", "$http",
+    function ($scope, $http) {
+      "use strict";
+      $scope.sendBlog = function () {
+        $http.post(
+          "$(API_URL)",
+          {
+            "request": "saveNewBlog",
+            "blog": {
+              "author": $scope.blog.author,
+              "title": $scope.blog.author,
+              "content": $scope.blog.content,
+              "metaDescription": $scope.blog.description,
+              "metaKeywords": $scope.blog.keywords
+            }
+          }
+        ).then(function successCallback() {
+          
+        }, function errorCallback() {
+          
+        });
+      };
     }]);
