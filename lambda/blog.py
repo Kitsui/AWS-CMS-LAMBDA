f7d24fe3998da4784a05e689f5ac39c36aaa126f
miggle <chownema@gmail.com>
2016-10-07 23:31:22 +1300

    Creating Blog post now populates paginated post list json for the visitor frontend

--- lambda/blog.py
+++ lambda/blog.py
@@ -101,6 +101,7 @@ class Blog(object):
                     "data": {"exception": str(e), "action": action}}
         
         # TODO: Add paging table
+        Blog.update_blog_list(blog_uuid, bucket)
         
         # Add blog as json to bucket
         try:
@@ -116,6 +117,69 @@ class Blog(object):
                     "data": {"exception": str(e), "action": action}}
         
         return {"message": "Successfully put blog", "data": blog}
+
+    @staticmethod
+    def generate_new_blog_list():
+        pass
+        
+
+    @staticmethod
+    def update_blog_list(blogID, bucket):
+        """ Puts a blog in the blog list file """
+        # Init Dict size
+        dictSize = 0
+        # Init set amount per array
+        maxSize = 10;
+        # file name of post list
+        fileName= "Content/post_list.json"
+        # Read in post list and put into json variable
+        try:
+            s3 = boto3.client('s3')
+            get_kwargs = {
+                'Bucket': bucket,
+                'Key': fileName
+            }
+            result =  s3.get_object(**get_kwargs)
+        except botocore.exceptions.ClientError as e:
+            action = "Putting blog id in the post list file"
+            return {"error": e.response["Error"]["Code"],
+                    "data": {"exception": str(e), "action": action}}
+        # Json variable
+        object_body = json.loads(result['Body'].read());
+        # set dict size
+        dictSize = len(object_body)
+        # If size is not 0
+        if object_body:
+            # Check size of each array
+            for key in object_body:
+                # Decrement Counter
+                dictSize-=1
+                print dictSize
+                # If check if space in array
+                if len(object_body[key]) < maxSize:
+                    object_body[key].append(blogID)
+                else:
+                    if dictSize == 0:
+                        # Else create new item with a value of key index + 1
+                        object_body[len(object_body)+1] = [blogID]
+                        break
+                
+
+        # Else create the first item in dictionary
+        else:
+            object_body[1] = [blogID]
+        # Add blog id as json to json file
+        try:
+            s3 = boto3.client("s3")
+            s3.put_object(
+                Bucket=bucket, ACL="public-read", Body=json.dumps(object_body),
+                Key=(fileName),
+                ContentType="application/json"
+            )
+        except botocore.exceptions.ClientError as e:
+            action = "Putting blog id in the post list file"
+            return {"error": e.response["Error"]["Code"],
+                    "data": {"exception": str(e), "action": action}}
         
     @staticmethod
     def delete_blog(blog_id, blog_table, bucket):
