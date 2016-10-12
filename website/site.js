7ba9b2ee3ef1e4584627d46241fd8ea7c128f3d7
John Cave <me@johncave.co.nz>
2016-10-13 11:01:31 +1300

    Added Post Listing
    Posts are now listed and visitor frontend is now complete except for
    navigation items.

--- website/site.js
+++ website/site.js
@@ -5,9 +5,15 @@ var app = angular.module('Kitsui', ['ngRoute'])
         
 		$routeProvider.when('/', {
            templateUrl: 'Templates/list.html', 
-           controller: 'HomeController'
+           controller: 'HomeController',
+		   controllerAs: 'home'
         });
 		
+		$routeProvider.when('/home/:page', {
+           templateUrl: 'Templates/list.html', 
+           controller: 'HomeController',
+		   controllerAs: 'home'
+        });
 		
         $routeProvider.when('/page/:pageid', {
             templateUrl: 'Templates/page.html', 
@@ -29,6 +35,20 @@ var app = angular.module('Kitsui', ['ngRoute'])
 	 
 app.filter('html', function($sce) { return $sce.trustAsHtml; });
 
+
+app.controller('SettingsController', [ '$http', '$rootScope', function($http, $rootScope){
+	var controller = this;
+	
+	$http.get("Content/site_settings.json").then(function(response){
+		for (var key in response.data){
+			controller[key] = response.data[key];
+		}
+		$rootScope.primary_colour = response.data.primary_colour;
+		$rootScope.highlight_colour = response.data.highlight_colour;
+		console.log($rootScope.primary_colour);
+	});
+	
+}]);
 
 
 app.controller('PageController', ['$http', '$routeParams', function($http, $routeParams){
@@ -61,23 +81,54 @@ app.controller('PostController', ['$http', '$routeParams', function($http, $rout
 } ]);
 
 //Handles displaying post.
-app.controller('HomeController', ['$http', '$location', function($http, $location){
+app.controller('HomeController', ['$http', '$routeParams', '$location', function($http, $routeParams, $location){
+	
+	// What page we are on of the home feed, if applicable.
+	var current_page = $routeParams.page;
+	if (!current_page){
+		current_page = 1;
+		console.log("There is no current page")
+	}
 	
 	var controller = this;
 	
-	$http.get("Content/post_list.json").then(function(response) {
+	$http.get("Content/post_list.json", {cache:false}).then(function(response) {
         
 		controller.posts = [];
-		for(i = 0; i < response.data.items.length; ++i){ // Loop through the post IDs and fetch their data.
-			$http.get("Content/Post/"+response.data.items[i]+".json").then(function(response) {
+		
+		// Get each post's JSON file.
+		for(i = 0; i < response.data[''+current_page].length; ++i){ // Loop through the post IDs and fetch their data.
+			$http.get("Content/Post/"+response.data[''+current_page][i]+".json").then(function(response) {
 				controller.posts.push(response.data);
 			});
 			
-			
 		}
 		
+		//Figure out if there's a next page
+		controller.next_page = parseInt(current_page) + 1;
 		console.log(controller);
+		// Check that there is a next page in the data
+		if(!response.data[controller.next_page]){
+			controller.next_page = null;
+			console.log("There is no next page: "+controller.next_page);
+		}
+		
+		//Figure out if there's a previous page
+		controller.previous_page = parseInt(current_page) - 1;
+		console.log(controller);
+		// Check that there is a previous page in the data
+		if(!response.data[controller.previous_page]){
+			controller.previous_page = null;
+			console.log("There is no previous page: "+controller.previous_page);
+		}
+		
+		
     });
+	
+	console.log("Current page: "+this.current_page);
+	console.log("Next page: "+this.next_page);
+	
+	console.log(this);
 	
 } ]);
 
